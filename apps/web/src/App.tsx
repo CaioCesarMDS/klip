88009516baee8893cc8a59c11888057b634086eb
CaioCesarMDS <caiocesarsts@gmail.com>
2026-05-06 19:04:40 -0300

    feat: initialize web application with Vite, React, and Tailwind CSS; add essential configuration files and components

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-yellow-400 w-full min-h-screen">
+      <h1 className="text-white text-center text-4xl font-bold py-4">
+        Klip APP
+      </h1>
+    </div>
+  );
+}
+
+export default App;
