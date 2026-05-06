2937c5947b8b586cd5591ac109cdf27ef6e3f349
CaioCesarMDS <caiocesarsts@gmail.com>
2026-05-05 22:04:59 -0300

    feat: integrate clips routes into the main application

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import { logger } from "@server/config/logger";
 import { initDatabase } from "@server/db";
 import { errorHandlerPlugin, openapiPlugin, requestLoggerPlugin } from "@server/plugins";
 import { Elysia } from "elysia";
+import { clipsRoutes } from "./module/clips/routes";
 
 export const app = new Elysia()
   .use(errorHandlerPlugin)
   .use(requestLoggerPlugin)
   .use(openapiPlugin)
+  .use(clipsRoutes)
   .use(await staticPlugin({ prefix: "/" }))
   .onStart(() => {
     initDatabase();
