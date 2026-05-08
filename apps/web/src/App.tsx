11e4c8323eb9031191fa4bddafa6699d81a457ef
CaioCesarMDS <caiocesarsts@gmail.com>
2026-05-08 11:54:45 -0300

    feat: refactor App component to use React Router for navigation

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,11 +1,23 @@
-function App() {
-  return (
-    <div className="bg-yellow-400 w-full min-h-screen">
-      <h1 className="text-white text-center text-4xl font-bold py-4">
-        Klip APP
-      </h1>
-    </div>
-  );
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AppLayout } from "@/components/layouts/AppLayout";
+import { CollectionsPage } from "@/pages/CollectionsPage";
+import { FavoritesPage } from "@/pages/FavoritesPage";
+import { HistoryPage } from "@/pages/HistoryPage";
+import { RecentsPage } from "@/pages/RecentsPage";
+import { SettingsPages } from "@/pages/SettingsPage";
 
-export default App;
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <AppLayout>
+        <Routes>
+          <Route path="/" element={<RecentsPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="/history" element={<HistoryPage />} />
+          <Route path="/collections" element={<CollectionsPage />} />
+          <Route path="/settings" element={<SettingsPages />} />
+        </Routes>
+      </AppLayout>
+    </BrowserRouter>
+  );
+};
